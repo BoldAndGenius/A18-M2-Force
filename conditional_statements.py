aba297fd1ff1f31d97d97eb14f315af5145bacff
SriLakshmi483 <lakkusrilakshmi483@gmail.com>
2024-09-23 23:33:06 +0530

    Added 43 program

--- conditional_statements.py
+++ conditional_statements.py
@@ -217,6 +217,15 @@ else:
     print(f"{num} is not divisible by 7")
     
 
+43.Write a program to check whether the year is leap year or not.
+year = int(input("Enter year:"))
+if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
+    print(f"{year} is a leap year")
+else:
+    print(f"{year} is not a leap year")
+    
+    
+
 44.Write a program to check whether a number entered is a 3 digit number or not.
 number = int(input("enter a number :"))
 if (number>99 and number<1000) or (number<-99 and number>-1000):
