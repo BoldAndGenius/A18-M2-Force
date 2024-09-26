1e58ef52dff99aadc03e48eb440ad2d618416561
Er. Kiran Kumar <kk9840042@gmail.com>
2024-09-26 18:58:21 +0530

    Swap Two Numbers with or without a Temporary Variable - Done

--- Additional Practice Programs [By Kiran Kumar]/26thsept(1).py
+++ Additional Practice Programs [By Kiran Kumar]/26thsept(1).py
@@ -19,3 +19,34 @@ Write a Python script that takes a user's age as input and prints whether they a
 Write a Python loop that prints the numbers from 1 to 10, but for multiples of 3, print "Fizz" instead of the number, and for multiples of 5, print "Buzz". If a number is a multiple of both 3 and 5, print "FizzBuzz".
 
 '''
+
+
+
+
+# Exchange the values of two numbers with & without using a temporary variable 
+# With Temporary Variable
+
+num1 = int(input("Enter a number 1 :"))
+num2 = int(input("Enter a number 2 :"))
+# color bucket example -- use this logic
+temp = num1
+num1 = num2
+num2 = temp
+print(f"The first number is {num1}")
+print(f"The second number is {num2}")
+
+
+
+# Without Temporary Variable
+# we can use Arithmetic Operation for that 
+num1 = int(input("Enter a number 1 :"))  # num1 = 10
+num2 = int(input("Enter a number 2 :"))  # num2 = 20
+
+num1 = num1 + num2  # num1 = 30  -- this is used for swapping!
+
+num2 = num1 - num2  # num2 = 30 - 20 = 10 
+num1 = num1 - num2  # num1 = 30 - 10 = 20
+# wow, swapping done! 
+
+print(f"The first number is {num1}")
+print(f"The second number is {num2}")
