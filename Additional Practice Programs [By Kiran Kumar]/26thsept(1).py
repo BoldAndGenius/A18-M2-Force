77bca9310a413584cb84e6bf8ee86c680a32bc52
Er. Kiran Kumar <kk9840042@gmail.com>
2024-09-26 19:25:38 +0530

    Average of Numbers in a List - Done

--- Additional Practice Programs [By Kiran Kumar]/26thsept(1).py
+++ Additional Practice Programs [By Kiran Kumar]/26thsept(1).py
@@ -23,8 +23,10 @@ Write a Python loop that prints the numbers from 1 to 10, but for multiples of 3
 
 
 
+# Question - 1
 # Exchange the values of two numbers with & without using a temporary variable 
-# With Temporary Variable
+
+# With Temporary Variable (Method - 1)
 
 num1 = int(input("Enter a number 1 :"))
 num2 = int(input("Enter a number 2 :"))
@@ -37,7 +39,7 @@ print(f"The second number is {num2}")
 
 
 
-# Without Temporary Variable
+# Without Temporary Variable (Method - 2)
 # we can use Arithmetic Operation for that 
 num1 = int(input("Enter a number 1 :"))  # num1 = 10
 num2 = int(input("Enter a number 2 :"))  # num2 = 20
@@ -50,3 +52,43 @@ num1 = num1 - num2  # num1 = 30 - 10 = 20
 
 print(f"The first number is {num1}")
 print(f"The second number is {num2}")
+
+
+
+
+
+# Question - 2
+# Calculate the average of numbers in a given list
+
+# first take elements of a list as in input in the list -- use for loop [easy]
+
+
+
+# Wrong Logic - First Try
+
+# total_elements = int(input("Enter the number of elements you want to enter :"))   # 3
+# list = []
+# for i in range(total_elements):    # it will from 0 to total_elements -- hence, total elments get it   # 0 to 2 it will go  # 0 # 1 # 2
+#     element = input("Enter an Element :")   # element = '10'  # element = '20' # element = '30'
+#     list.append(element)   # list = ['10']  # list = ['10', '20']  # list = ['10', '20', '30']
+# # print(list) - the user entered list got printed 
+# sum_of_elements = sum(list)
+# print(sum_of_elements)
+# average = sum_of_elements / total_elements
+# print(f"The Average of elements in a {list} is {average}")
+
+# Why my logic was wrong is....because I was using sum function on the string elements. It means, I need to typecaste it into 
+
+
+
+# Try 2
+
+total_elements = int(input("Enter the number of elements you want to enter :"))
+list1 = []
+for i in range(total_elements):  
+    element = int(input("Enter an Element :"))
+    list1.append(element)
+# print(list1) - for testing
+sum_of_elements = sum(list1)
+average = sum_of_elements / total_elements
+print(f"The Average of Elements in a {list1} is {average} ")
