36c0347bb296c0e4d8fe525087f003f3575e7680
Er. Kiran Kumar <kk9840042@gmail.com>
2024-09-26 16:27:32 +0530

    Assignment - 2 (While Loop) Questions Added

---
+++ Additional Practice Programs [By Kiran Kumar]/26thsept(1).py
@@ -0,0 +1,21 @@
+'''
+
+List Comprehensions
+
+Question: Write a Python list comprehension that generates a list of all numbers between 1 and 100 that are divisible by both 3 and 5.
+Bonus: Modify the comprehension to include only those numbers where the sum of their digits is greater than 5.
+
+
+
+Create a Python list containing the numbers 1 to 5. Write a script to print the list in reverse order.
+
+
+Given a string s = "Hello, World!", write a Python script to convert the string to all lowercase letters and then print it.
+
+
+Write a Python script that takes a user's age as input and prints whether they are a child (age < 18), an adult (18 ≤ age < 65), or a senior (age ≥ 65).
+
+
+Write a Python loop that prints the numbers from 1 to 10, but for multiples of 3, print "Fizz" instead of the number, and for multiples of 5, print "Buzz". If a number is a multiple of both 3 and 5, print "FizzBuzz".
+
+'''
