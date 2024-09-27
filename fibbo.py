fd218d5e5d836769f571ba73a94e6c6f1336ec65
vishalmishra311 <vishalmishra4400@gmail.com>
2024-09-27 18:55:08 +0530

    Fibonacci numbers

---
+++ fibbo.py
@@ -0,0 +1,12 @@
+# Fibonacci numbers
+num=int(input("please enter a number : "))
+a,b,sums=0,1,0
+print(a)
+while num-1:
+    a=b
+    b=sums
+    sums=a+b
+    print(a+b)
+    num-=1
+   
+# VISHAL MISHRA A18 
