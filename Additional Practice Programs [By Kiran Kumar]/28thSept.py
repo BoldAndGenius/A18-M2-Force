06c444ec01cdd9b9513996f02e8b86518f89212d
Er. Kiran Kumar <kk9840042@gmail.com>
2024-09-28 20:42:06 +0530

    Sum of Even Numbers Up to a Given Number n

--- Additional Practice Programs [By Kiran Kumar]/28thSept.py
+++ Additional Practice Programs [By Kiran Kumar]/28thSept.py
@@ -14,3 +14,19 @@ for element in numbers:
         # print(element) # it will print all the positive numbers, but I want a count of it, therefore initialise a count variable with 0 & everytime update by 1
         count = count + 1
 print(f"The Number of Positive Numbers in the list are {count}")
+
+
+
+# Question - 2
+# Sum of Even Numbers 
+# Problem - Calculate the sum of even numbers up to a given number n
+
+
+given_number = int(input("Enter any number :"))   # 10
+sum = 0
+for num in range(0,given_number+1,2):
+    print(num)  # what are those numbers   # 0,2,4,6,8,10
+    sum = sum + num  # 30
+print(f"The sum of even numbers upto {given_number} is {sum}")
+
+
