b15f04dc0b62247859c78ff611c78c1eb127439e
Er. Kiran Kumar <kk9840042@gmail.com>
2024-10-01 10:19:05 +0530

    Extract lowercase characters from a string

--- Additional Practice Programs [By Kiran Kumar]/1stOct.py
+++ Additional Practice Programs [By Kiran Kumar]/1stOct.py
@@ -7,3 +7,15 @@ for item in collection:
     if type(item) in [int,float,complex]:
         numeric_collection.append(item)
 print(f"The Numeric Collection is : {numeric_collection}")
+
+
+
+
+# Extract lowercase characters from a string
+
+string = input("Enter any string : ") #"kIran"
+lowercase = ""
+for characters in string:
+    if characters.islower():
+        lowercase = lowercase + characters   # ''+'k' = 'k'
+print(f"The lowercase characters are : {lowercase}")
