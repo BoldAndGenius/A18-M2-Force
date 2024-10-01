17dbf0f541ebd6624add71c2defe0bd41f8a34cc
Er. Kiran Kumar <kk9840042@gmail.com>
2024-10-01 12:09:47 +0530

    Display a Dictionary whose Keys are Words of a Sentence and Values are Lenght of each word using for loop

--- Additional Practice Programs [By Kiran Kumar]/1stOct.py
+++ Additional Practice Programs [By Kiran Kumar]/1stOct.py
@@ -19,3 +19,35 @@ for characters in string:
     if characters.islower():
         lowercase = lowercase + characters   # ''+'k' = 'k'
 print(f"The lowercase characters are : {lowercase}")
+
+
+
+
+
+# Display a Dictionary whose Keys are Words of a Sentence and Values are Lenght of each word using for loop.
+
+sentence = input("Enter any sentence : ")  #kiran is a good boy
+words_list = sentence.split()  # ['kiran', 'is', 'a', 'good', 'boy']   -- list of words
+# print(word)
+# word = ['kiran','is', 'a', 'good', 'boy']
+dictionary = {}
+for word in words_list:   # apply for loop on the list of word that we split it
+    # print(word)
+    if word not in dictionary:
+        dictionary[word] = len(word)    #if not present this key value pair, create this key value pair
+
+print(dictionary)
+
+
+
+
+
+
+sentence = input("Enter any sentence :")
+words_list = sentence.split()
+# print(words_list)
+dictionary = {}
+for word in words_list:
+    if word not in dictionary:
+        dictionary[word] = len(word)
+print(dictionary)
