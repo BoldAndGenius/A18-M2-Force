79baa4daf3e7c2a52b69dc822ca336f45b1200fa
Er. Kiran Kumar <kk9840042@gmail.com>
2024-10-02 09:27:42 +0530

    Type of Character Entered is - Uppercase, Lowercase, Numbers or Special Characters (Using Method -1)

--- Additional Practice Programs [By Kiran Kumar]/2ndOct.py
+++ Additional Practice Programs [By Kiran Kumar]/2ndOct.py
@@ -5,3 +5,18 @@ if len(collection) <= 5:
     print("The entered collection has less than or equal to 5 Elements")
 else:
     print("The enterd collection doesn't has less than or equal to 5 Elements")
+    
+
+
+
+# Type of Character Entered is - Uppercase, Lowercase, Numbers or Special Characters
+
+character = input("Enter any character : ")
+if 'A' <= character <= "Z":
+    print("Uppercase")
+elif 'a' <= character <= 'z':
+    print("Lowercase")
+elif '0' <= character <= '9':
+    print("Numbers")
+else:
+    print("Special Characters")
