8c7d0c1ceff22bf7b13849d649d3c73330a1addd
Er. Kiran Kumar <kk9840042@gmail.com>
2024-10-02 11:49:28 +0530

    Assignment-2 Qno.7 - Done

---
+++ Assignment -2 (While Loop) [By Kiran Kumar]/2ndOcto.py
@@ -0,0 +1,35 @@
+''' 
+Q1. Write a program to print the following using while loop
+a. First 10 Even numbers
+b. First 10 Odd numbers
+c. First 10 Natural numbers
+d. First 10 Whole numbers
+Q2. Write a program to print first 10 integers and their squares using while loop.
+Q3. Write while loop statement to print the following series: 10, 20, 30 … … 300
+Q4. Write a while loop statement to print the following series 105, 98, 91 ………7.
+Q5. Write a program to print first 10 natural number in reverse order using while loop.
+Q6. Write a program to print sum of first 10 Natural numbers.
+
+
+Q9. Write a program to print all even numbers that falls between two numbers (exclusive both
+numbers) entered from the user using while loop.
+
+
+
+
+
+'''
+
+
+# Q7. Write a program to print sum of first 10 Even numbers.
+# num = int(input("Enter a number for which you want to print the even numbers :"))
+num = 18
+sum = 0
+for i in range(0,num+1,2):
+    print(i) #- for validation 
+    sum = sum + i
+print(f"The Sum of first 10 even numbers is ",sum)
+
+
+
+
