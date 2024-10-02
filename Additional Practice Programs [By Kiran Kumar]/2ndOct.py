5831a8a05ac5e748d5e05db36e0bbce7ce3e13e7
Er. Kiran Kumar <kk9840042@gmail.com>
2024-10-02 09:20:35 +0530

    Check Entered Collection has less than or equal to 5 elements or not

---
+++ Additional Practice Programs [By Kiran Kumar]/2ndOct.py
@@ -0,0 +1,7 @@
+# Check Entered Collection has less than or equal to 5 elements or not
+
+collection = eval(input("Enter any collection : "))
+if len(collection) <= 5:
+    print("The entered collection has less than or equal to 5 Elements")
+else:
+    print("The enterd collection doesn't has less than or equal to 5 Elements")
