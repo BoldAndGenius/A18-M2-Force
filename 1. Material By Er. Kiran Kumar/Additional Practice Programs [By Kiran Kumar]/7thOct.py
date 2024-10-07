d79fe15348dae8cfdaabfb10c1fbf8d82db944b5
Er. Kiran Kumar <kk9840042@gmail.com>
2024-10-07 21:54:23 +0530

    Print F Pattern

--- 1. Material By Er. Kiran Kumar/Additional Practice Programs [By Kiran Kumar]/7thOct.py
+++ 1. Material By Er. Kiran Kumar/Additional Practice Programs [By Kiran Kumar]/7thOct.py
@@ -76,3 +76,16 @@ for row in range(n):
         else:
             print(" ",end=" ")
     print()
+    
+    
+    
+# Print F
+
+n = int(input())
+for row in range(n):
+    for col in range(n):
+        if row == 0  or  col == 0  or row == n//2:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
