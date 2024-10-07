39e20d98c5cf8c2dcaa92f9efb0a4ed81055c946
ManojReddy-ui <manojreddy9835@gmail.com>
2024-10-07 21:50:28 +0530

    added patterns

--- Manoj Reddy/Alpha Patterns.py
+++ Manoj Reddy/Alpha Patterns.py
@@ -1,244 +1,244 @@
-# # #base programs of Patterns
+# #base programs of Patterns
 
-# for row in range(5):
-#     for col in range(5):
-#         print((row,col),end=' ')
-#     print()
+for row in range(5):
+    for col in range(5):
+        print((row,col),end=' ')
+    print()
     
-# for row in range(5):
-#     for col in range(5):
-#         if row==col:
-#             print('*',end=' ')
-#         else:
-#             print(' ',end=' ')
-#     print()
+for row in range(5):
+    for col in range(5):
+        if row==col:
+            print('*',end=' ')
+        else:
+            print(' ',end=' ')
+    print()
 
-# ## C Pattern
+## C Pattern
  
-# n=int(input("Enter Number: "))
-# for row in range(n):
-#     for col in range(n):
-#         if row == 0 or row == n-1 or col == 0:
-#             print('C',end = ' ')
-#         else:
-#             print(' ',end=' ')
-#     print()
+n=int(input("Enter Number: "))
+for row in range(n):
+    for col in range(n):
+        if row == 0 or row == n-1 or col == 0:
+            print('C',end = ' ')
+        else:
+            print(' ',end=' ')
+    print()
 
 
-# ## E Pattern
+## E Pattern
 
-# n=int(input("Enter Number: "))
-# for row in range(n):
-#     for col in range(n):
-#         if col == 0 or row == 0 or row == n-1 or row == n//2:
-#             print('E',end = ' ')
-#         else:
-#             print(' ',end=' ')
-#     print()
+n=int(input("Enter Number: "))
+for row in range(n):
+    for col in range(n):
+        if col == 0 or row == 0 or row == n-1 or row == n//2:
+            print('E',end = ' ')
+        else:
+            print(' ',end=' ')
+    print()
     
-#  ## F Pattern
+ ## F Pattern
 
-# n=int(input("Enter Number: "))
-# for row in range(n):
-#     for col in range(n):
-#         if col == 0 or row == 0 or row == n//2:
-#             print('F',end = ' ')
-#         else:
-#             print(' ',end=' ')
-#     print()   
+n=int(input("Enter Number: "))
+for row in range(n):
+    for col in range(n):
+        if col == 0 or row == 0 or row == n//2:
+            print('F',end = ' ')
+        else:
+            print(' ',end=' ')
+    print()   
 
-# ## G Pattern
+## G Pattern
 
-# n=int(input("Enter Number: "))
-# for row in range(n):
-#     for col in range(n):
-#         if col == 0 or row == 0 or row == n-1 or (row == n//2 and col>=n//2) or (col == n-1 and row >= n//2):
-#             print('G',end = ' ')
-#         else:
-#             print(' ',end=' ')
-#     print()
+n=int(input("Enter Number: "))
+for row in range(n):
+    for col in range(n):
+        if col == 0 or row == 0 or row == n-1 or (row == n//2 and col>=n//2) or (col == n-1 and row >= n//2):
+            print('G',end = ' ')
+        else:
+            print(' ',end=' ')
+    print()
     
-# ## H Pattern
+## H Pattern
 
-# n=int(input("Enter Number: "))
-# for row in range(n):
-#     for col in range(n):
-#         if col == 0 or col == n-1 or row==n//2 :
-#             print('H',end = ' ')
-#         else:
-#             print(' ',end=' ')
-#     print()
+n=int(input("Enter Number: "))
+for row in range(n):
+    for col in range(n):
+        if col == 0 or col == n-1 or row==n//2 :
+            print('H',end = ' ')
+        else:
+            print(' ',end=' ')
+    print()
 
-# ## I Pattern
+## I Pattern
 
-# n=int(input("Enter Number: "))
-# for row in range(n):
-#     for col in range(n):
-#         if row == 0 or row == n-1 or col == n//2:
-#             print('I',end = ' ')
-#         else:
-#             print(' ',end=' ')
-#     print()
+n=int(input("Enter Number: "))
+for row in range(n):
+    for col in range(n):
+        if row == 0 or row == n-1 or col == n//2:
+            print('I',end = ' ')
+        else:
+            print(' ',end=' ')
+    print()
 
-# ## J Pattern
+## J Pattern
 
-# n=int(input("Enter Number: "))
-# for row in range(n):
-#     for col in range(n):
-#         if row == 0 or col == n//2  or (col==0 and row>=n//2) or (row==n-1 and col <= n//2):
-#             print('J',end = ' ')
-#         else:
-#             print(' ',end=' ')
-#     print()
+n=int(input("Enter Number: "))
+for row in range(n):
+    for col in range(n):
+        if row == 0 or col == n//2  or (col==0 and row>=n//2) or (row==n-1 and col <= n//2):
+            print('J',end = ' ')
+        else:
+            print(' ',end=' ')
+    print()
     
-# ## L Pattern
+## L Pattern
 
-# n=int(input("Enter Number: "))
-# for row in range(n):
-#     for col in range(n):
-#         if col == 0 or row == n-1:
-#             print('L',end = ' ')
-#         else:
-#             print(' ',end=' ')
-#     print()
+n=int(input("Enter Number: "))
+for row in range(n):
+    for col in range(n):
+        if col == 0 or row == n-1:
+            print('L',end = ' ')
+        else:
+            print(' ',end=' ')
+    print()
 
-# ## M Pattern 
+## M Pattern 
 
-# n=int(input("Enter Number: "))
-# for row in range(n):
-#     for col in range(n):
-#         if col == 0 or col == n-1 or (row==col and row<=n//2) or (row+col==(n-1) and row<=n//2):
-#             print('M',end = ' ')
-#         else:
-#             print(' ',end=' ')
-#     print()
+n=int(input("Enter Number: "))
+for row in range(n):
+    for col in range(n):
+        if col == 0 or col == n-1 or (row==col and row<=n//2) or (row+col==(n-1) and row<=n//2):
+            print('M',end = ' ')
+        else:
+            print(' ',end=' ')
+    print()
 
-# ## N Pattern
+## N Pattern
 
-# n=int(input("Enter Number: "))
-# for row in range(n):
-#     for col in range(n):
-#         if col == 0 or row == col or col == n-1:
-#             print('N',end = ' ')
-#         else:
-#             print(' ',end=' ')
-#     print()
+n=int(input("Enter Number: "))
+for row in range(n):
+    for col in range(n):
+        if col == 0 or row == col or col == n-1:
+            print('N',end = ' ')
+        else:
+            print(' ',end=' ')
+    print()
 
 
-# ## O Pattern
+## O Pattern
 
-# n=int(input("Enter Number: "))
-# for row in range(n):
-#     for col in range(n):
-#         if col == 0 or row == 0 or row == n-1 or col == n-1:
-#             print('O',end = ' ')
-#         else:
-#             print(' ',end=' ')
-#     print()
+n=int(input("Enter Number: "))
+for row in range(n):
+    for col in range(n):
+        if col == 0 or row == 0 or row == n-1 or col == n-1:
+            print('O',end = ' ')
+        else:
+            print(' ',end=' ')
+    print()
 
-# ## P Pattern
+## P Pattern
 
-# n=int(input("Enter Number: "))
-# for row in range(n):
-#     for col in range(n):
-#         if col == 0 or row == 0 or row == n//2 or (col==n-1 and row <= n//2):
-#             print('P',end = ' ')
-#         else:
-#             print(' ',end=' ')
-#     print()
+n=int(input("Enter Number: "))
+for row in range(n):
+    for col in range(n):
+        if col == 0 or row == 0 or row == n//2 or (col==n-1 and row <= n//2):
+            print('P',end = ' ')
+        else:
+            print(' ',end=' ')
+    print()
 
-# ## R Pattern
+## R Pattern
 
-# n=int(input("Enter Number: "))
-# for row in range(n):
-#     for col in range(n):
-#         if col == 0 or row == 0 or row == n//2 or (col==n-1 and row <= n//2) or (row==col and row >= n//2):
-#             print('R',end = ' ')
-#         else:
-#             print(' ',end=' ')
-#     print()
+n=int(input("Enter Number: "))
+for row in range(n):
+    for col in range(n):
+        if col == 0 or row == 0 or row == n//2 or (col==n-1 and row <= n//2) or (row==col and row >= n//2):
+            print('R',end = ' ')
+        else:
+            print(' ',end=' ')
+    print()
 
-# ## S Pattern
+## S Pattern
 
-# n=int(input("Enter Number: "))
-# for row in range(n):
-#     for col in range(n):
-#         if row == 0 or row == n-1 or row == n//2 or (col==0 and row<=n//2) or (col==n-1 and row >= n//2) :
-#             print('S',end = ' ')
-#         else:
-#             print(' ',end=' ')
-#     print()
+n=int(input("Enter Number: "))
+for row in range(n):
+    for col in range(n):
+        if row == 0 or row == n-1 or row == n//2 or (col==0 and row<=n//2) or (col==n-1 and row >= n//2) :
+            print('S',end = ' ')
+        else:
+            print(' ',end=' ')
+    print()
     
-# ## T Pattern
+## T Pattern
 
-# n=int(input("Enter Number: "))
-# for row in range(n):
-#     for col in range(n):
-#         if row == 0 or col == n//2 :
-#             print('T',end = ' ')
-#         else:
-#             print(' ',end=' ')
-#     print()
+n=int(input("Enter Number: "))
+for row in range(n):
+    for col in range(n):
+        if row == 0 or col == n//2 :
+            print('T',end = ' ')
+        else:
+            print(' ',end=' ')
+    print()
 
-# ## U Pattern
+## U Pattern
 
-# n=int(input("Enter Number: "))
-# for row in range(n):
-#     for col in range(n):
-#         if col == 0 or col == n-1 or row == n-1 :
-#             print('U',end = ' ')
-#         else:
-#             print(' ',end=' ')
-#     print()
+n=int(input("Enter Number: "))
+for row in range(n):
+    for col in range(n):
+        if col == 0 or col == n-1 or row == n-1 :
+            print('U',end = ' ')
+        else:
+            print(' ',end=' ')
+    print()
 
-## V Pattern
-
-
-
-# ## W Pattern
-
-# n=int(input("Enter Number: "))
-# for row in range(n):
-#     for col in range(n):
-#         if col == 0 or col == n-1 or (row==col and row>=n//2) or (row+col==(n-1) and row>=n//2):
-#             print('W',end = ' ')
-#         else:
-#             print(' ',end=' ')
-#     print()
-
-# ## X Pattern
-
-# n=int(input("Enter Number: "))
-# for row in range(n):
-#     for col in range(n):
-#         if row == col or row+col == n-1:
-#             print('X',end = ' ')
-#         else:
-#             print(' ',end=' ')
-#     print()
-
-# ## Y Pattern
-
-# n=int(input("Enter Number: "))
-# for row in range(n):
-#     for col in range(n):
-#         if (row == col and row <= n//2) or (row+col == n-1 and row <= n//2) or (col == n//2 and row >= n//2) :
-#             print('Y',end = ' ')
-#         else:
-#             print(' ',end=' ')
-#     print()
+# V Pattern
 
 
-# ## Z Pattern
 
-# n=int(input("Enter Number: "))
-# for row in range(n):
-#     for col in range(n):
-#         if row==0 or row== n-1 or (row+col==n-1):
-#             print('Z',end = ' ')
-#         else:
-#             print(' ',end=' ')
-#     print()
+## W Pattern
+
+n=int(input("Enter Number: "))
+for row in range(n):
+    for col in range(n):
+        if col == 0 or col == n-1 or (row==col and row>=n//2) or (row+col==(n-1) and row>=n//2):
+            print('W',end = ' ')
+        else:
+            print(' ',end=' ')
+    print()
+
+## X Pattern
+
+n=int(input("Enter Number: "))
+for row in range(n):
+    for col in range(n):
+        if row == col or row+col == n-1:
+            print('X',end = ' ')
+        else:
+            print(' ',end=' ')
+    print()
+
+## Y Pattern
+
+n=int(input("Enter Number: "))
+for row in range(n):
+    for col in range(n):
+        if (row == col and row <= n//2) or (row+col == n-1 and row <= n//2) or (col == n//2 and row >= n//2) :
+            print('Y',end = ' ')
+        else:
+            print(' ',end=' ')
+    print()
+
+
+## Z Pattern
+
+n=int(input("Enter Number: "))
+for row in range(n):
+    for col in range(n):
+        if row==0 or row== n-1 or (row+col==n-1):
+            print('Z',end = ' ')
+        else:
+            print(' ',end=' ')
+    print()
     
 
     
