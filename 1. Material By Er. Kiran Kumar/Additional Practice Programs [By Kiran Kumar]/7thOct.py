141c47375c3e3416b2e182d2cc7e53425f2794dd
Er. Kiran Kumar <kk9840042@gmail.com>
2024-10-07 20:21:56 +0530

    Print O Pattern

--- 1. Material By Er. Kiran Kumar/Additional Practice Programs [By Kiran Kumar]/7thOct.py
+++ 1. Material By Er. Kiran Kumar/Additional Practice Programs [By Kiran Kumar]/7thOct.py
@@ -40,3 +40,15 @@ for row in range(n):
         else:
             print(" ",end=" ")
     print()
+    
+    
+# Print O 
+
+n = int(input())
+for row in range(n):
+    for col in range(n):
+        if row==0 or row==n-1 or col==0 or col==n-1:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
