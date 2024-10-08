e1666adba592723b2f3d9efdd3118c7bc19c0f61
ShraddhaNeeli <shraddhaneeli@gmail.com>
2024-10-07 22:44:52 -0700

    Update pattern_printing.py

--- shraddha_neeli/pattern_printing.py
+++ shraddha_neeli/pattern_printing.py
@@ -24,7 +24,7 @@ for row in range(n25):
 
 # -------------------- letter X ------------------------
 
-n24 = int(input("enter a odd number for letter Y : "))
+n24 = int(input("enter a odd number for letter X : "))
 
 for row in range(n24):
     for col in range(n24):
@@ -41,9 +41,9 @@ n23 = int(input("enter a odd number for letter W : "))
 for row in range(n23):
     for col in range(n23):
         if col == 0 or col == n23 - 1 or (row == col and row >= n23 // 2) or (row + col == n23 - 1 and row >= n23 // 2):
-            print("*", end="")
+            print("*", end=" ")
         else:
-            print(" ", end="")
+            print(" ", end=" ")
     print()
 
 # -------------------- letter V ------------------------
@@ -91,13 +91,13 @@ for row in range(n20):
 n19 = int(input("enter a odd number for letter S : "))
 
 for row in range(n19):
-    for col in range(n19 ):
-        if (row == 0 or row == n19 - 1 or row == n19 // 2) and n19 // 2 > col > 0 :
+    for col in range(n19):
+        if (row == 0 or row == n19 - 1 or row == n19 // 2) and n19 - 2 > col > 0 :
             print("* ", end="")
         elif (col == 0 and 0 < row < n19 // 2) or (col == n19 - 2  and n19 - 1 > row > n19 // 2):
-            print("*", end="")
+            print("*", end=" ")
         else:
-            print(" ", end="")
+            print(" ", end=" ")
     print()
 
 # -------------------- letter R ------------------------
@@ -106,12 +106,12 @@ n18 = int(input("enter a odd number for letter  R: "))
 
 for row in range(n18):
     for col in range(n18):
-        if (row == 0 or row == n18 // 2) and (1 < col < n18 // 2 + 1):
+        if (row == 0 or row == n18 // 2) and (col < n18 - 1):
             print("* ", end="")
         elif (col == 0 or (col == n18 - 1 and row <= n18 // 2 - 1)) and (0 < row <= n18-1) or (row - col == n18 // 2 - 1 and row > n18 // 2):
-            print("*", end="")
+            print("*", end=" ")
         else:
-            print(" ", end="")
+            print(" ", end=" ")
     print()
 
 # -------------------- letter Q ------------------------
@@ -134,12 +134,12 @@ n16 = int(input("enter a odd number for letter P : "))
 
 for row in range(n16):
     for col in range(n16):
-        if (row == 0 or row == n16 // 2) and (1 < col < n16 // 2 + 1):
+        if (row == 0 or row == n16 // 2) and (col < n16 - 1):
             print("* ", end="")
-        elif (col == 0 or (col == n16 - 1 and row <= n16 // 2 - 1)) and (0 < row < n16-1):
-            print("*", end="")
+        elif (col == 0 or (col == n16 - 1 and 0 < row <= n16 // 2 - 1)):
+            print("*", end=" ")
         else:
-            print(" ", end="")
+            print(" ", end=" ")
     print()
 
 # -------------------- letter O ------------------------
@@ -163,9 +163,9 @@ n14 = int(input("enter a odd number for letter N : "))
 for row in range(n14):
     for col in range(n14):
         if col == 0 or col == n14 - 1 or (row == col):
-            print("*", end="")
+            print("*", end=" ")
         else:
-            print(" ", end="")
+            print(" ", end=" ")
     print()
 
 # -------------------- letter M ------------------------
@@ -175,9 +175,9 @@ n13 = int(input("enter a odd number for letter M : "))
 for row in range(n13):
     for col in range(n13):
         if col == 0 or col == n13 - 1 or (row == col and row <= n13 // 2) or (row + col == n13 - 1 and row <= n13 // 2):
-            print("*", end="")
+            print("*", end=" ")
         else:
-            print(" ", end="")
+            print(" ", end=" ")
     print()
 
 # -------------------- letter L ------------------------
@@ -201,9 +201,9 @@ n11= int(input("enter a odd number for letter K : "))
 for row in range(n11):
     for col in range(n11):
         if col == 0 or ((row + col == n11 // 2) and (col > 0)) or ((row - col == n11 // 2) and (col > 0)):
-            print("*", end="")
+            print("*", end=" ")
         else:
-            print(" ", end="")
+            print(" ", end=" ")
     print()
 
 # -------------------- letter J ------------------------
@@ -213,9 +213,9 @@ n10= int(input("enter a odd number for letter J : "))
 for row in range(n10):
     for col in range(n10):
         if (col == n10 // 2 and row < n10 - 1) or row == 0 or (col == 0 and n10 // 2 < row < n10 -1) or (row == n10 -1 and 0 < col < n10 // 2):
-            print("*", end="")
+            print("*", end=" ")
         else:
-            print(" ", end="")
+            print(" ", end=" ")
     print()
 
 # -------------------- letter I ------------------------
@@ -225,9 +225,9 @@ n9= int(input("enter a odd number for letter I : "))
 for row in range(n9):
     for col in range(n9):
         if col == n9 // 2 or row == 0 or row == n9 - 1:
-            print("*", end="")
+            print("*", end=" ")
         else:
-            print(" ", end="")
+            print(" ", end=" ")
     print()
 
 # -------------------- letter H ------------------------
@@ -237,9 +237,9 @@ n8= int(input("enter a odd number for letter H : "))
 for row in range(n8):
     for col in range(n8):
         if col == 0 or col == n8-1 or row == n8 // 2:
-            print("*", end="")
+            print("*", end=" ")
         else:
-            print(" ", end="")
+            print(" ", end=" ")
     print()
 
 # -------------------- letter G ------------------------
@@ -259,12 +259,10 @@ n6= int(input("enter a odd number for letter F : "))
 
 for row in range(n6):
     for col in range(n6):
-        if col == 0:
-            print("*", end="")
-        elif (row == 0 or row == n6 // 2) and col > 1:
-            print("* ", end="")
+        if col == 0 or (row == 0 or row == n6 // 2):
+            print("*", end=" ")
         else:
-            print(" ", end="")
+            print(" ", end=" ")
     print()
 
 # -------------------- letter E ------------------------
@@ -273,12 +271,10 @@ n5= int(input("enter a odd number for letter E : "))
 
 for row in range(n5):
     for col in range(n5):
-        if col == 0:
-            print("*", end="")
-        elif (row == 0 or row == n5 // 2 or row == n5 - 1) and col > 1:
-            print("* ", end="")
+        if col == 0 or (row == 0 or row == n5 // 2 or row == n5 - 1):
+            print("*", end=" ")
         else:
-            print(" ", end="")
+            print(" ", end=" ")
     print()
 
 # -------------------- letter D ------------------------
@@ -286,13 +282,11 @@ for row in range(n5):
 n4 = int(input("enter a odd number for letter D : "))
 
 for row in range(n4):
-    for col in range(n4 * 2 - 1):
-        if col == 0 or (col == n4 * 2 - 2 and 0 < row < n4 - 1):
-            print("*", end="")
-        elif (row == 0 or row == n4 - 1) and (1 < col < n4):
-            print("* ", end="")
+    for col in range(n4):
+        if col == 0 or (col == n4 - 1 and 0 < row < n4 - 1) or ((row == 0 or row == n4 - 1) and (0 < col < n4 - 1)):
+            print("*", end=" ")
         else:
-            print(" ", end="")
+            print(" ", end=" ")
     print()
 
 # -------------------- letter C ------------------------
@@ -301,12 +295,10 @@ n3= int(input("enter a odd number for letter C : "))
 
 for row in range(n3):
     for col in range(n3):
-        if col == 0 and 0 < row < n3 - 1:
-            print("*", end="")
-        elif (row == 0 or row == n3 - 1) and col != 0:
-            print("* ", end="")
+        if (col == 0 and 0 < row < n3 - 1) or ((row == 0 or row == n3 - 1) and col > 0):
+            print("*", end=" ")
         else:
-            print(" ", end="")
+            print(" ", end=" ")
     print()
 
 # -------------------- letter B ------------------------
@@ -315,10 +307,10 @@ n2 = int(input("enter a odd number for letter B : "))
 
 for row in range(n2):
     for col in range(n2):
-        if (col == 0 and 0 < row < n2 - 1) or (col == n2 - 1 and 0 < row < n2 - 1 and row != n2 // 2) or (row == 0 and col < n2 - 4) or (row == n2 - 1 and col < n2 - 4) or (row == n2 // 2 and col < n2 - 4):
+        if (col == 0 and 0 < row < n2 - 1) or (col == n2 - 1 and 0 < row < n2 - 1 and row != n2 // 2) or (row == 0 and col < n2 -1) or (row == n2 - 1 and col < n2 -1) or (row == n2 // 2 and col < n2 - 1):
             print("*", end=" ")
         else:
-            print(" ", end="")
+            print(" ", end=" ")
     print()
 
 # ----------------------- letter A --------------------------
