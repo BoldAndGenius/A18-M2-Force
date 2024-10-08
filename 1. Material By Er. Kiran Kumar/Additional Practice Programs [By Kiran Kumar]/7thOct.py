a9767acff8f52c8030cc200ad9cc7304e08667ba
Er. Kiran Kumar <kk9840042@gmail.com>
2024-10-08 12:11:25 +0530

    Print N Pattern

--- 1. Material By Er. Kiran Kumar/Additional Practice Programs [By Kiran Kumar]/7thOct.py
+++ 1. Material By Er. Kiran Kumar/Additional Practice Programs [By Kiran Kumar]/7thOct.py
@@ -124,3 +124,15 @@ for row in range(n):
             print(" ",end=" ")
     print()
     
+
+
+# Print N
+ 
+n = int(input())
+for row in range(n):
+    for col in range(n):
+        if col == 0 or col==n-1 or row==col:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
