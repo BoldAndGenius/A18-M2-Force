48df98adb517f3f36b20c5504638f4ef2db77fe3
Er. Kiran Kumar <kk9840042@gmail.com>
2024-10-08 19:12:55 +0530

    Left Aligned Triangle Pattern

--- 1. Material By Er. Kiran Kumar/Additional Practice Programs [By Kiran Kumar]/8thOct.py
+++ 1. Material By Er. Kiran Kumar/Additional Practice Programs [By Kiran Kumar]/8thOct.py
@@ -36,3 +36,30 @@ for row in range(n):
         else:
             print(" ",end=" ")
     print()
+    
+    
+    
+# Solid Pattern -
+
+''' 
+
+*         
+* *       
+* * *     
+* * * *   
+* * * * * 
+
+Left Aligned Triangle Pattern
+'''
+
+n = int(input())
+for row in range(n):
+    for col in range(n):
+        if col==0 or row==n-1 or row==col or row>col:
+            print("*",end=" ") 
+        else:
+            print(" ",end=" ")
+    print()
+
+
+    
