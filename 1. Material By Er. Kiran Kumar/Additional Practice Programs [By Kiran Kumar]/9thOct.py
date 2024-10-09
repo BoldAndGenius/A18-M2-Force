fca704e85981cbaaf9a2784445851cb4249762fd
Er. Kiran Kumar <kk9840042@gmail.com>
2024-10-09 20:35:36 +0530

    Number Pattern - 2 : Done

--- 1. Material By Er. Kiran Kumar/Additional Practice Programs [By Kiran Kumar]/9thOct.py
+++ 1. Material By Er. Kiran Kumar/Additional Practice Programs [By Kiran Kumar]/9thOct.py
@@ -15,3 +15,25 @@ for row in range(1,n+1):
     for col in range(row):
         print(col+1, end=" ")
     print()
+    
+    
+    
+'''
+
+1
+2 3
+4 5 6
+7 8 9 10
+
+
+'''
+
+n = 5
+start = 1
+for row in range(n):
+    for col in range(row):
+        print(start, end=" ")
+        start = start + 1
+    print()
+        
+        
