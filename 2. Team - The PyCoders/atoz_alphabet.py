f99f621d2678b620b24cb9a92aa01a53b91aa23e
Er. Kiran Kumar <kk9840042@gmail.com>
2024-10-11 14:40:33 +0530

    X Alphabet - The PyCoders Team

--- 2. Team - The PyCoders/atoz_alphabet.py
+++ 2. Team - The PyCoders/atoz_alphabet.py
@@ -937,3 +937,47 @@ for row in range(n):
         else:
             print(" ",end=" ")
     print()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+'''
+X Alphabet
+
+*                   * 
+  *               *   
+    *           *     
+      *       *       
+        *   *         
+          *           
+        *   *         
+      *       *       
+    *           *     
+  *               *   
+*                   * 
+
+
+'''
+n = int(input())  # 11
+for row in range(n):
+    for col in range(n):
+        if row==col or row+col==n-1:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
