d38d40786a1f476c67466ee0c9cc30bfffd1a1b6
Er. Kiran Kumar <kk9840042@gmail.com>
2024-10-11 12:07:30 +0530

    I Alphabet - The PyCoders Team

--- 2. Team - The PyCoders/atoz_alphabet.py
+++ 2. Team - The PyCoders/atoz_alphabet.py
@@ -322,3 +322,42 @@ for row in range(n):
         else:
             print(" ",end=" ")
     print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+I Alphabet
+
+* * * * * * * * * * * 
+          *           
+          *           
+          *           
+          *           
+          *           
+          *           
+          *           
+          *           
+          *           
+* * * * * * * * * * * 
+
+
+'''
+
+n = int(input())  # 10
+for row in range(n):
+    for col in range(n):
+        if row==0 or row==n-1 or col==n//2:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
