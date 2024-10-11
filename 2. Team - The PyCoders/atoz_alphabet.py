116ed1d563ad0dc3eff2b82cc917584b9a5ddcde
Er. Kiran Kumar <kk9840042@gmail.com>
2024-10-11 14:44:02 +0530

    Y Alphabet - The PyCoders Team

--- 2. Team - The PyCoders/atoz_alphabet.py
+++ 2. Team - The PyCoders/atoz_alphabet.py
@@ -981,3 +981,51 @@ for row in range(n):
         else:
             print(" ",end=" ")
     print()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+"""
+Y Alphabet
+
+*                   * 
+  *               *   
+    *           *     
+      *       *       
+        *   *         
+          *           
+          *           
+          *           
+          *           
+          *           
+          *           
+
+
+"""
+
+n = int(input())  # 11
+for row in range(n):
+    for col in range(n):
+        if (row==col and col<=n//2) or (row+col==n-1 and col>=n//2) or (col==n//2 and row>=n//2):
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
