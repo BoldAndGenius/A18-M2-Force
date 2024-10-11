e86d16795371594b1b7230c75b7da59a71ff4370
Er. Kiran Kumar <kk9840042@gmail.com>
2024-10-11 16:10:22 +0530

    Qno. 86 - Done

--- 1. Material By Er. Kiran Kumar/Assignment -1 (If Else) [By Kiran Kumar]/11thOcto.py
+++ 1. Material By Er. Kiran Kumar/Assignment -1 (If Else) [By Kiran Kumar]/11thOcto.py
@@ -7,3 +7,19 @@ elif '0'<character<='9':
     print("Digits.")
 else:
     print("Special Characters")
+    
+    
+ 
+# Question No. 86   
+# Write a program to consider an integer number. Print happy if the number is divisible by two. Print SAD if the number is divisible by 5 and print square of the numbers only if it is divisible by seven else print the number as it is.
+
+
+number = int(input('Enter any number : '))
+if number%2 == 0:
+    print("Happy.")
+if number%5==0:
+    print("Sad.")
+if number%7==0:
+    print(number*number)
+else:
+    print(number)
