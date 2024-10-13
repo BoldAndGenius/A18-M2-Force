3d8b5858b6ea8bd8ce197bdee934168edd727c45
aishu11092001 <aishwaryakt01@gmail.com>
2024-10-13 13:21:59 +0530

    pattern

--- Team Assignments/THE_NUCLEONS/team_assignment-1.py
+++ Team Assignments/THE_NUCLEONS/team_assignment-1.py
@@ -1,1 +1,8 @@
-print("hello Mates")
+n = int(input("enter a number : "))
+for row in range(n):
+    for col in range(n):
+        if (row >= col and row <= n//2) or (row + col >= n-1 and col <= n//2) or (row <= col and row >= n//2) or (row + col <= n-1 and col >= n//2 ):
+            print("*",end = " ")
+        else:
+            print(" ",end = " ")    
+    print()        
