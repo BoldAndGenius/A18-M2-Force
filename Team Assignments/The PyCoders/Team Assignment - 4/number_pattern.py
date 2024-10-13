331fa1bbf6cedef0624bbefa9e0f4c8a20049d01
Er. Kiran Kumar <kk9840042@gmail.com>
2024-10-13 20:04:42 +0530

    Number Pattern - 2 : Repeat Value 1 in Inverted Left Aligned Right Angle Triangle  - The PyCoders Team

--- Team Assignments/The PyCoders/Team Assignment - 4/number_pattern.py
+++ Team Assignments/The PyCoders/Team Assignment - 4/number_pattern.py
@@ -1,5 +1,5 @@
 '''
-Number Pattern - 1
+Number Pattern - 1 : Repeat Value 1 
 
 1 
 1 1 
@@ -17,3 +17,33 @@ for row in range(n):
     for col in range(row+1):
         print("1",end=" ")
     print()
+    
+    
+    
+    
+    
+    
+    
+
+
+'''
+Number Pattern - 2 : Repeat Value 1 in Inverted Left Aligned Right Angle Triangle 
+
+1 1 1 1 1 1 1 
+1 1 1 1 1 1 
+1 1 1 1 1 
+1 1 1 1 
+1 1 1 
+1 1 
+1 
+
+
+'''
+
+
+n = int(input())  # 7
+for row in range(n):
+    for col in range(row,n):
+        print("1",end=" ")
+    print()
+
