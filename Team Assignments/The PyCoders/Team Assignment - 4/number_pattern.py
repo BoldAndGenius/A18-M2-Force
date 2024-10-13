bfc987a9539041679eef7b444b70622874aa0b52
Er. Kiran Kumar <kk9840042@gmail.com>
2024-10-13 20:52:02 +0530

    Number Pattern - 5 : Repeat Value 1 in Reverse Hill Pattern - The PyCoders Team

--- Team Assignments/The PyCoders/Team Assignment - 4/number_pattern.py
+++ Team Assignments/The PyCoders/Team Assignment - 4/number_pattern.py
@@ -140,3 +140,31 @@ for row in range(n):
     for col in range(row+1):
         print("1",end=" ")
     print()
+    
+    
+    
+    
+    
+    
+'''
+Number Pattern - 5 : Repeat Value 1 in Reverse Hill Pattern
+
+  1 1 1 1 1 1 1 1 1 1 1 1 1 
+    1 1 1 1 1 1 1 1 1 1 1 
+      1 1 1 1 1 1 1 1 1 
+        1 1 1 1 1 1 1 
+          1 1 1 1 1 
+            1 1 1 
+              1 
+              
+'''
+
+n = int(input())  # 7
+for row in range(n):
+    for col in range(row+1):
+        print(" ",end=" ")
+    for col in range(row,n-1):
+        print("1",end=" ")
+    for col in range(row,n):
+        print("1",end=" ")
+    print()
