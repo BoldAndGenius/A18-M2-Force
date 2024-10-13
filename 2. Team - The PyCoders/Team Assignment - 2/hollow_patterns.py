f7da922cc560f13eff9fbc9f20a5dcdf2d8007ec
Er. Kiran Kumar <kk9840042@gmail.com>
2024-10-13 09:55:21 +0530

    Hollow Square - The PyCoders Team

---
+++ 2. Team - The PyCoders/Team Assignment - 2/hollow_patterns.py
@@ -0,0 +1,24 @@
+'''
+Hollow Square
+
+* * * * * * * * * * 
+*                 * 
+*                 * 
+*                 * 
+*                 * 
+*                 * 
+*                 * 
+*                 * 
+*                 * 
+* * * * * * * * * * 
+
+'''
+
+n = int(input())
+for row in range(n):
+    for col in range(n):
+        if row==0 or row==n-1 or col==0 or col==n-1:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
