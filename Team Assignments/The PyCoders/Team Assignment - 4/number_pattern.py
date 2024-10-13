f65eaa0dbef612323ca795809c9ee81ad86834ab
Er. Kiran Kumar <kk9840042@gmail.com>
2024-10-13 20:31:14 +0530

    Number Pattern - 4 : Repeat Value 1 in Right Aligned Inverted Right Angle Triangle - The PyCoders Team

--- Team Assignments/The PyCoders/Team Assignment - 4/number_pattern.py
+++ Team Assignments/The PyCoders/Team Assignment - 4/number_pattern.py
@@ -75,3 +75,31 @@ for row in range(n):
     for col in range(row+1):
         print("1",end=" ")
     print()
+    
+    
+    
+    
+    
+    
+    
+
+
+'''
+Number Pattern - 4 : Repeat Value 1 in Right Aligned Inverted Right Angle Triangle
+
+  1 1 1 1 1 1 1 
+    1 1 1 1 1 1 
+      1 1 1 1 1 
+        1 1 1 1 
+          1 1 1 
+            1 1 
+              1 
+'''
+
+n = int(input()) # 7
+for row in range(n):
+    for col in range(row+1):
+        print(" ",end=" ")
+    for col in range(row,n):
+        print("1",end=" ")
+    print()
