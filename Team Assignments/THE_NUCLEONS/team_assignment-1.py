a801b3551920c1fcb1d9717779659194df01727f
aishu11092001 <aishwaryakt01@gmail.com>
2024-10-13 13:25:34 +0530

    pattern

--- Team Assignments/THE_NUCLEONS/team_assignment-1.py
+++ Team Assignments/THE_NUCLEONS/team_assignment-1.py
@@ -1,4 +1,4 @@
-n = int(input("enter a number : "))
+n = int(input("enter a number :"))
 for row in range(n):
     for col in range(n):
         if (row >= col and row <= n//2) or (row + col >= n-1 and col <= n//2) or (row <= col and row >= n//2) or (row + col <= n-1 and col >= n//2 ):
