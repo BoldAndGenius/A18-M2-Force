90b029ce060b72da00d1625444c181026bd609d6
Er. Kiran Kumar <kk9840042@gmail.com>
2024-10-14 18:39:23 +0530

    Alphabet Pattern - 3

--- Team Assignments/The PyCoders/Team Assignment - 5/alphabet_pattern.py
+++ Team Assignments/The PyCoders/Team Assignment - 5/alphabet_pattern.py
@@ -51,3 +51,33 @@ for row in range(n):
         print(char,end=" ")
         char = chr(ord(char)+1)
     print()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+'''
+Alphabet Pattern - 3
+
+A B C D E 
+F G H I J 
+K L M N O 
+P Q R S T 
+U V W X Y 
+
+'''
+
+char = 'A'
+n = int(input())
+for row in range(n):
+    for col in range(n):
+        print(char,end=" ")
+        char = chr(ord(char)+1)
+    print()
