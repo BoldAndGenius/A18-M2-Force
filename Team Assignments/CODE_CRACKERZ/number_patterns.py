875f9ff640d87198408dabf60179c494c04b709c
Praneetha154 <praneethakonduru2003@gmail.com>
2024-10-14 17:22:23 +0530

    updated number_pattern

--- Team Assignments/CODE_CRACKERZ/number_patterns.py
+++ Team Assignments/CODE_CRACKERZ/number_patterns.py
@@ -126,5 +126,26 @@ for row in range(1,num+1):
     print()
 
 
+'''
+Pattern-8
+1 2 3 4 5
+  6 7 8 9
+    1 2 3
+      4 5
+        6
+'''
+num =int(input("Enter a number:"))
+count=1
+for row in range(num,0,-1):
+    print("  " * (num-row),end=" ")
+    for col in range(row):
+        if count > 9:
+            count =1
+        print(count,end=" ")
+        count+=1
+    print()
+
+
+
 
 
