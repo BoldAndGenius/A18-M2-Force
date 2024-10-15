eeb5d32553e1e937333ad11f7814021cbc03e298
diyanaik1 <137207019+diyanaik1@users.noreply.github.com>
2024-10-15 11:52:15 +0530

    Update ALPHABETS.py

--- Team Assignments/CODE_COMMANDERS/ALPHABETS.py
+++ Team Assignments/CODE_COMMANDERS/ALPHABETS.py
@@ -366,10 +366,8 @@ for row in range(n):
 n=7
 for row in range(n):
     for col in range(n):
-        if (row==0 or row==n-2) and (col>0 and col<n-1) or (col==0 or col==n-1) and (row>0 and row<n-2):
+        if (row==0 or row==n-2) and (col>0 and col<n-1) or (col==0 or col==n-1) and (row>0 and row<n-2) or  row==col and row>=n//2::
             print('*',end=' ') 
-        elif row==col and row>=n//2:
-            print('*',end=' ')
         else:
             print(' ',end=' ')
     print()
@@ -391,7 +389,7 @@ for row in range(n):
 n=7
 for row in range(n):
     for col in range(n):
-        if col==0 or (row==0 or row==n//2) and col<n-1 or col==n-1 and row<n//2 or (row==col and row>n//2):
+        if col==0 or (row==0 or row==n//2) or (col==n-1 and row<n//2) or (row==col and row>n//2):
             print('*',end=' ') 
         else:
             print(' ',end=' ')
@@ -402,7 +400,7 @@ for row in range(n):
 * * * * * * *
 *           *
 *           *
-* * * * * *
+* * * * * * *
 *       *
 *         *
 *           *
