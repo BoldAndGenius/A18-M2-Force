88daf0ef70bf1deb8ba457337d5f792eb0e1fe54
Er. Kiran Kumar <kk9840042@gmail.com>
2024-10-16 23:19:48 +0530

    Function to check a Given Number is Odd or Even

--- Contributions/1. Material By Er. Kiran Kumar/Additional Practice Programs [By Kiran Kumar]/16thOct.py
+++ Contributions/1. Material By Er. Kiran Kumar/Additional Practice Programs [By Kiran Kumar]/16thOct.py
@@ -1,3 +1,5 @@
+
+# Function to Check a Given String is Palindrome or Not
 def palindrome_checker(string):
     reverse = string[::-1]
     if string == reverse:
@@ -5,4 +7,16 @@ def palindrome_checker(string):
     else:
         return "Not a Palindrome"
 check = palindrome_checker("mam")
+print(check)
+
+
+
+# Function to check a Given Number is Odd or Even
+
+def check_odd_even(num):
+    if num % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+check = check_odd_even(5)
 print(check)
