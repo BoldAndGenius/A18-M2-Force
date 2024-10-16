1bc1a1b6a1070fcd786de3d146c7280f3ceddd60
MUKESH333RS <mukeshmukesh10035@gmail.com>
2024-10-16 18:30:56 +0530

    add while loop folder

--- Team Assignments/God_Of_Codes/While_loops.py
+++ Team Assignments/God_Of_Codes/While_loops.py
@@ -38,3 +38,125 @@ while index < len(list_collection):
 
     index += 1
 print(result)
+
+""" 
+
+76.Write a program to get the following output
+input1=’11001010’
+input2=’01110010’
+output=4(to count how many positions are having same value)
+from itertools import count, product
+"""
+
+# input1="11001010"
+# input2="01110010"
+# count = 0
+# output = 0
+# while count < len(input1):
+#     if input1[count] == input2[count]:
+#         output += 1
+#     count += 1
+# print(output)
+
+"""
+
+77..Write a program to get the following output
+input=[1,2,3,4,5,6]
+value=3
+output=[1,2,3][4,5,6]
+If value=2
+output=[1,2][3,4][5,6]
+"""
+# ip=[1,2,3,4,5,6]
+# value = int(input("Enter a value:"))
+# count = 0
+# index = 0
+# output = []
+# while count < len(ip) // value:
+#     list1=[]
+#     in_index = 0
+#     while in_index < value:
+#         list1.append(ip[index])
+#         in_index += 1
+#         index += 1
+#     output.append(list1)
+#     count += 1
+# print(output)
+
+"""
+
+78.Write a program to check weather the given number is spy number or not i.e, 1*2*3=1+2+3
+"""
+# ip = int(input("Enter a number:"))
+# num = ip
+# sum = 0
+# prodt = 1
+# while num > 0:
+#     digit = num % 10
+#     sum += digit
+#     prodt *= digit
+#     num //= 10
+# if sum == prodt:
+#     print(f"{ip} is a spy number")
+# else:
+#     print(f"{ip} is not a spy number")
+"""
+
+79..Write a program to check weather the given number is Xylem number or not i.e, 1234 →
+1+4=2+3
+"""
+# ip = input("Enter a number:")
+# dig = [int(ip[i]) for i in range(len(ip))]
+# Extreme_dig = []
+# Mid_dig = []
+# Sum_of_Extreme_dig = 0
+# Sum_of_Mid_dig = 0
+# index = 0
+# while index < len(dig):
+#     if index == 0 or index == len(dig) - 1:
+#         Extreme_dig.append(dig[index])
+#     else:
+#         Mid_dig.append(dig[index])
+#     index += 1
+# index = 0
+# while index < len(Extreme_dig):
+#     Sum_of_Extreme_dig += Extreme_dig[index]
+#     index += 1
+# index = 0
+# while index < len(Mid_dig):
+#     Sum_of_Mid_dig += Mid_dig[index]
+#     index += 1
+# if Sum_of_Extreme_dig == Sum_of_Mid_dig:
+#     print(f"{ip} is a Xylem number.")
+# else:
+#     print(f"{ip} is not a Xylem number.")
+"""
+
+80.Write a program to check weather the given number is phloem number or not
+I.e, 12345 → 1+5 != 2+3+4
+"""
+# ip = input("Enter a number:")
+# dig = [int(ip[i]) for i in range(len(ip))]
+# Extreme_dig = []
+# Mid_dig = []
+# Sum_of_Extreme_dig = 0
+# Sum_of_Mid_dig = 0
+# index = 0
+# while index < len(dig):
+#     if index == 0 or index == len(dig) - 1:
+#         Extreme_dig.append(dig[index])
+#     else:
+#         Mid_dig.append(dig[index])
+#     index += 1
+# index = 0
+# while index < len(Extreme_dig):
+#     Sum_of_Extreme_dig += Extreme_dig[index]
+#     index += 1
+# index = 0
+# while index < len(Mid_dig):
+#     Sum_of_Mid_dig += Mid_dig[index]
+#     index += 1
+# if Sum_of_Extreme_dig != Sum_of_Mid_dig:
+#     print(f"{ip} is a Phloem number.")
+# else:
+#     print(f"{ip} is not a Phloem number.")
