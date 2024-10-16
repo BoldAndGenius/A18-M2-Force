aa56e863910dc2379feb9525cc7b5ee78835c518
ManojReddy-ui <manojreddy9835@gmail.com>
2024-10-16 22:09:53 +0530

    update D pattern

--- Contributions/Manoj Reddy/Alpha Patterns.py
+++ Contributions/Manoj Reddy/Alpha Patterns.py
@@ -52,7 +52,7 @@ n = int(input("Enter Number: "))
 for row in range(n):
     for col in range(n):
         if col == 0 or (row == 0 or row == n-1) and col != n-1 or col == n-1 and row != 0 and row != n-1:
-            print("*",end=" ")
+            print("D",end=" ")
         else:
             print(" ",end=" ")
     print()
