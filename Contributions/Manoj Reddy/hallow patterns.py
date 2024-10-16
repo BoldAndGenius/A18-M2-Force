bc4a47466164c0aab4ba2ec3db401117cb19451f
Manoj Reddy C <manojreddy9835@gmail.com>
2024-10-16 21:08:59 +0530

    Update hallow patterns.py

--- Contributions/Manoj Reddy/hallow patterns.py
+++ Contributions/Manoj Reddy/hallow patterns.py
@@ -1,26 +1,19 @@
-# # Hallow square pattern
-
-# n = int(input("Enter Number: "))
-# for row in range(n):
-#     for col in range(n):
-#         if row in (0,n-1) or col in (0,n-1):
-#             print('*',end=' ')
-#         else:
-#             print(' ',end=' ')
-#     print()
-    
-# '''     * * * * * 
-#         *       *
-#         *       *
-#         *       *
-#         * * * * *        '''
+# Hallow square pattern
 
 n = int(input("Enter Number: "))
 for row in range(n):
-    for col in range(n//2):
-        print('*',end=' ')
-    else:
-        print(' ',end=' ')
+    for col in range(n):
+        if row in (0,n-1) or col in (0,n-1):
+            print('*',end=' ')
+        else:
+            print(' ',end=' ')
     print()
-    print()
+    
+'''     * * * * * 
+        *       *
+        *       *
+        *       *
+        * * * * *        '''
+
+
         
