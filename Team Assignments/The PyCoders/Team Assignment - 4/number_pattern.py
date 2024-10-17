9305c6cfcbc28695d67ae518c660b76752b832fd
Er. Kiran Kumar <kk9840042@gmail.com>
2024-10-17 21:18:14 +0530

    Number Pattern - 43 : Floyd Triangle

--- Team Assignments/The PyCoders/Team Assignment - 4/number_pattern.py
+++ Team Assignments/The PyCoders/Team Assignment - 4/number_pattern.py
@@ -1390,3 +1390,25 @@ for row in range(n):
     print(num,end=" ")
     num = num - 1
   print()
+
+
+
+
+
+'''
+Number Pattern - 43 : Floyd Triangle
+
+1 
+2 3 
+4 5 6 
+7 8 9 10 
+11 12 13 14 15 
+
+'''
+n = int(input())
+num = 1
+for row in range(n):
+  for col in range(row+1):
+    print(num,end=" ")
+    num = num + 1
+  print()
