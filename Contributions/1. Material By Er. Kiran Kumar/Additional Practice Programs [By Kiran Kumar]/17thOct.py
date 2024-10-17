222f44f4399fad1b73f3b565d495bad65bc02a92
Er. Kiran Kumar <kk9840042@gmail.com>
2024-10-17 17:58:01 +0530

    Define a Function which returns the square of a number if it is even else cube of it.

--- Contributions/1. Material By Er. Kiran Kumar/Additional Practice Programs [By Kiran Kumar]/17thOct.py
+++ Contributions/1. Material By Er. Kiran Kumar/Additional Practice Programs [By Kiran Kumar]/17thOct.py
@@ -12,3 +12,12 @@ def user_credentials():
     username = input("Enter your Username : ")
     password = input("Enter your Password : ")
     print(f"Your Username is {username} and Your Password is {password}")
+    
+    
+# Define a Function which returns the square of a number if it is even else cube of it.
+def get_square_or_cube():
+    num = int(input("Enter any Number : "))
+    if num % 2 == 0:
+        return num ** 2
+    else:
+        return num ** 3
