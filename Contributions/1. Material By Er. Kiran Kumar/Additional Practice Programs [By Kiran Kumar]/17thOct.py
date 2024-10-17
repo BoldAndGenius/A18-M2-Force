cd417fecb910cdfda730837d5799f41ead6f2c11
Er. Kiran Kumar <kk9840042@gmail.com>
2024-10-17 17:55:47 +0530

    Define a Function which asks for a Username & Password and print it.

--- Contributions/1. Material By Er. Kiran Kumar/Additional Practice Programs [By Kiran Kumar]/17thOct.py
+++ Contributions/1. Material By Er. Kiran Kumar/Additional Practice Programs [By Kiran Kumar]/17thOct.py
@@ -4,3 +4,11 @@ def add():
     num1 = int(input("Enter the number 1 : "))
     num2 = int(input("Enter the number 2 : "))
     print(f"The sum of {num1} and {num2} is {num1+num2}")
+    
+    
+    
+# Define a Function which asks for a Username & Password and print it.
+def user_credentials():
+    username = input("Enter your Username : ")
+    password = input("Enter your Password : ")
+    print(f"Your Username is {username} and Your Password is {password}")
