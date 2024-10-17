3d1094b6ce3adbb1625b44571e4225707dcc5497
Er. Kiran Kumar <kk9840042@gmail.com>
2024-10-17 21:27:45 +0530

    Square Parallel Bar Pattern

--- Team Assignments/The PyCoders/Team Assignment - 2/hollow_patterns.py
+++ Team Assignments/The PyCoders/Team Assignment - 2/hollow_patterns.py
@@ -380,3 +380,32 @@ for row in range(n):
       print(" ",end=" ")
   print()
     
+    
+    
+    
+    
+    
+    
+    
+    
+  
+  
+  
+'''
+Square Parallel Bar Pattern 
+
+*       * 
+*       * 
+*       * 
+*       * 
+*       * 
+
+'''
+n = int(input())
+for row in range(n):
+  for col in range(n):
+    if col==0 or col==n-1:
+      print("*",end=" ")
+    else:
+      print(" ",end=" ")
+  print()
