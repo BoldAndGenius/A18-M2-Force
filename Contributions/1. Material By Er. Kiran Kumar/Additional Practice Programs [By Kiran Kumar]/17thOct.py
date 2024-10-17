3036d48b53e3463cf4cae8e157d724c76fd64838
Er. Kiran Kumar <kk9840042@gmail.com>
2024-10-17 17:52:32 +0530

    Define a Function which adds two number

---
+++ Contributions/1. Material By Er. Kiran Kumar/Additional Practice Programs [By Kiran Kumar]/17thOct.py
@@ -0,0 +1,6 @@
+# Define a Function which adds two number 
+
+def add():
+    num1 = int(input("Enter the number 1 : "))
+    num2 = int(input("Enter the number 2 : "))
+    print(f"The sum of {num1} and {num2} is {num1+num2}")
