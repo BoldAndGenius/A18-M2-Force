af54563a8f74003106aa968448163fbd70f96e63
Er. Kiran Kumar <kk9840042@gmail.com>
2024-10-18 23:43:52 +0530

    function taking function name as an argument (Program - 2)

--- Contributions/1. Material By Er. Kiran Kumar/Additional Practice Programs [By Kiran Kumar]/18thOct.py
+++ Contributions/1. Material By Er. Kiran Kumar/Additional Practice Programs [By Kiran Kumar]/18thOct.py
@@ -1,7 +1,9 @@
 
 
-# function taking function name as an argument 
+# function taking function name as an argument (Program - 1)
 
+
+# Each time you create a function, id has been assigned.
     # 0x1   # assign 0x2
 def demo(func_name):
     print("In Demo")
@@ -21,4 +23,37 @@ demo(sample)
 In Demo
 In Sample
 
+'''
+
+
+
+
+
+
+
+
+
+# function taking function name as an argument (Program - 2)
+
+# Each time you create a function, id has been assigned.
+  # 0x1  # 0x3 as been assigned
+def demo(func):
+    print("In Demo")
+       # 0x2
+    def inner():
+        print("In Inner")
+        func()
+         
+    return inner   # it will return 0x2 address tot the calling function
+    # 0x3
+def greet():
+    print("Hello World") 
+
+# 0x1 # 0x3
+demo(greet)
+
+'''
+Output -
+
+In Demo
 '''
