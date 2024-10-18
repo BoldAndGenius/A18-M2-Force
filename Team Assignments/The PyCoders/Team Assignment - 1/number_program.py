ac1419494357d6e8a8b19e202fa20b7366922bb3
Er. Kiran Kumar <kk9840042@gmail.com>
2024-10-18 08:35:01 +0530

    All Fancy Number Program - 1 : Check if a number is Palindrome or not

--- Team Assignments/The PyCoders/Team Assignment - 1/number_program.py
+++ Team Assignments/The PyCoders/Team Assignment - 1/number_program.py
@@ -5,3 +5,21 @@ while num>0:
     last = num % 10 
     print(last)
     num = num // 10
+    
+    
+    
+# All Fancy Number Program - 1 : Check if a number is Palindrome or not 
+
+num = int(input("Enter a number : "))
+temp = num
+reverse = 0
+
+while num>0:
+    last = num % 10 
+    reverse = reverse + num 
+    num = num // 10
+if reverse == temp:
+    print("Palindrome Number.")
+else:
+    print("Not a Palindrome.")
+    
