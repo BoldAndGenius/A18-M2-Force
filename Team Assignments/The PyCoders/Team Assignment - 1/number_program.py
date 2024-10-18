5300065f6dba7bef9191b9ac24d9235f24bdd6f6
Er. Kiran Kumar <kk9840042@gmail.com>
2024-10-18 08:40:05 +0530

    All Fancy Number Program - 2 : Check if a number is Spy Number or Not

--- Team Assignments/The PyCoders/Team Assignment - 1/number_program.py
+++ Team Assignments/The PyCoders/Team Assignment - 1/number_program.py
@@ -23,3 +23,21 @@ if reverse == temp:
 else:
     print("Not a Palindrome.")
     
+    
+# All Fancy Number Program - 2 : Check if a number is Spy Number or Not 
+# Spy Number =>  sum of it's digits == product of it's digits
+# Eg. 123 is a Spy Number =>   1+2+3 = 6   and 1*2*3 = 6     6 == 6
+
+num = int(input("Enter any number: "))
+sum = 0
+product = 1
+while num>0:
+    last = num % 10 
+    sum = sum + last
+    product = product * last
+    num = num // 10
+if sum == product:
+    print("Spy Number.")
+else:
+    print("Not a Spy Number.")
+    
