0670feefda087b5758e31da27df387ae21634fd0
Er. Kiran Kumar <kk9840042@gmail.com>
2024-10-20 03:05:24 +0530

    Factor Program - 2 : Find if a Number is a Prime Number or not.

--- Team Assignments/The PyCoders/Team Assignment - 1/factor_program.py
+++ Team Assignments/The PyCoders/Team Assignment - 1/factor_program.py
@@ -14,3 +14,39 @@ Output -
 The Factors are :  1 2 3 5 6 10 15 30 
 
 '''
+
+
+
+
+
+
+# Factor Program - 2 : Find if a Number is a Prime Number or not.
+
+# A Prime Number is a number that divisible by only 1 & itself. So, it have only two factors.
+# 5  -- 5 is divisible by only 1 & 5. 
+
+
+num = int(input("Enter a Number : "))
+count = 0
+for factor in range(2,num):
+    if num % factor == 0:
+        count = count + 1
+if count == 0:
+    print("Prime Number.")
+else:
+    print("Not a Prime Number.")
+    
+    
+    
+
+
+
+num = int(input("Enter a Number : "))
+count = 0
+for factor in range(1,num+1):
+    if num % factor == 0:
+        count = count + 1
+if count == 2:
+    print("Prime Number.")
+else:
+    print("Not a Prime Number.")
