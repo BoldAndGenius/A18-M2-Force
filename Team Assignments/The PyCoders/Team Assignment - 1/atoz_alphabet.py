cec604c9d34425141570e73e6331c926757a3c90
Er. Kiran Kumar <kk9840042@gmail.com>
2024-10-21 02:12:25 +0530

    Making R Alphabet Curvy

--- Team Assignments/The PyCoders/Team Assignment - 1/atoz_alphabet.py
+++ Team Assignments/The PyCoders/Team Assignment - 1/atoz_alphabet.py
@@ -812,12 +812,24 @@ R Alphabet
 *                   * 
 
 
+
+* * * * * *   
+*           * 
+*           * 
+* * * * * *   
+*       *     
+*         *   
+*           * 
+
+
 '''
 
 n = int(input())  # 12
 for row in range(n):
     for col in range(n):
-        if row == 0 or col==0 or row==n//2 or (col==n-1 and row<=n//2) or (row==col and row>=n//2):
+        if (row==0 and col==n-1) or (row==n//2 and col==n-1):
+            print(" ",end=" ")
+        elif row == 0 or col==0 or row==n//2 or (col==n-1 and row<=n//2) or (row==col and row>=n//2):
             print("*",end=" ")
         else:
             print(" ",end=" ")
