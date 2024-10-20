07649fde22f16952c2393c1b7711fa9858744304
Er. Kiran Kumar <kk9840042@gmail.com>
2024-10-21 01:51:38 +0530

    Making B Alphabet Curvy

--- Team Assignments/The PyCoders/Team Assignment - 1/atoz_alphabet.py
+++ Team Assignments/The PyCoders/Team Assignment - 1/atoz_alphabet.py
@@ -84,12 +84,20 @@ B Alphabet
 * * * * * * * 
 
 
+* * * *   
+*       * 
+* * * * * 
+*       * 
+* * * *  
+
 '''
 
 n = int(input())  # 10
 for row in range(n):
     for col in range(n):
-        if col==0 or row==0 or row==n-1 or (col==n-1 and row<=n/2) or row==n//2 or (col==n-1 and row>=n//2):
+        if (row==0 and col==n-1) or (row==n-1 and col==n-1):
+            print(" ",end=" ")
+        elif col==0 or row==0 or row==n-1 or (col==n-1 and row<=n/2) or row==n//2 or (col==n-1 and row>=n//2):
             print("*",end=" ")
         else:
             print(" ",end=" ")
