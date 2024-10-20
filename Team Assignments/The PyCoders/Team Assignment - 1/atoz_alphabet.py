fb6a3ffab476e3e20470e642d5d8448b9ffb0574
Er. Kiran Kumar <kk9840042@gmail.com>
2024-10-21 02:15:49 +0530

    Making S Alphabet Curvy

--- Team Assignments/The PyCoders/Team Assignment - 1/atoz_alphabet.py
+++ Team Assignments/The PyCoders/Team Assignment - 1/atoz_alphabet.py
@@ -868,12 +868,23 @@ S Alphabet
 * * * * * * * * * * * 
 
 
+  * * * * * * 
+*             
+*             
+  * * * * *   
+            * 
+            * 
+* * * * * *  
+
+
 '''
 
 n = int(input())  #10
 for row in range(n):
     for col in range(n):
-        if row==0 or row==n-1 or row==n//2 or (col==0 and row<=n//2) or (col==n-1 and row>=n//2):
+        if (row==0 and col==0) or (col==0 and row==n//2) or (row==n//2 and col==n-1) or (col==n-1 and row==n-1):
+            print(" ",end=" ")
+        elif row==0 or row==n-1 or row==n//2 or (col==0 and row<=n//2) or (col==n-1 and row>=n//2):
             print("*",end=" ")
         else:
             print(" ",end=" ")
