78cb1d48898fd7540960bc9a63b5fa0e5681684c
Er. Kiran Kumar <kk9840042@gmail.com>
2024-10-21 02:08:06 +0530

    Making P Alphabet Curvy

--- Team Assignments/The PyCoders/Team Assignment - 1/atoz_alphabet.py
+++ Team Assignments/The PyCoders/Team Assignment - 1/atoz_alphabet.py
@@ -708,13 +708,22 @@ P Alphabet
 *              
 
 
+* * * * * *   
+*           * 
+*           * 
+* * * * * *   
+*             
+*             
+* 
 
 '''
 
 n = int(input())  # 11
 for row in range(n):
     for col in range(n):
-        if col==0 or row==0 or (col==n-1 and row<=n//2) or row==n//2:
+        if (row==0 and col==n-1) or (row==n//2 and col==n-1):
+            print(" ",end=" ")
+        elif col==0 or row==0 or (col==n-1 and row<=n//2) or row==n//2:
             print("*",end=" ")
         else:
             print(" ",end=" ")
