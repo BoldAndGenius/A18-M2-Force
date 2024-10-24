1f21d2a523c98cf4b026d825e3a4128bc367ad57
Er. Kiran Kumar <kk9840042@gmail.com>
2024-10-25 01:25:36 +0530

    Program to add two numbers Using Predefined Variables

---
+++ Contributions/1. Material By Er. Kiran Kumar/Additional Practice Programs [By Kiran Kumar]/25thOct.py
@@ -0,0 +1,12 @@
+# Revision - Solve 100 Programming Questions in 9 Hours 
+
+# Revision Series - Program to add two numbers. 
+
+# Using Predefined Variables
+num1 = 10
+num2 = 30
+sum = num1 + num2
+print(f"Sum of {num1} + {num2} is {sum}")
+
+
+# Using Taking User Input 
