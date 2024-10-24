374f55fd986a801ab3f86e27bff34246c961d04d
Er. Kiran Kumar <kk9840042@gmail.com>
2024-10-25 02:12:31 +0530

    Python Program to Print Hello World

--- Contributions/1. Material By Er. Kiran Kumar/Additional Practice Programs [By Kiran Kumar]/25thOct.py
+++ Contributions/1. Material By Er. Kiran Kumar/Additional Practice Programs [By Kiran Kumar]/25thOct.py
@@ -1,5 +1,6 @@
 # Revision - Solve 100 Programming Questions in 9 Hours 
 
+# Question - 1
 # Revision Series - Program to add two numbers. 
 
 # Using Predefined Variables
@@ -9,9 +10,16 @@ sum = num1 + num2
 print(f"Sum of {num1} + {num2} is {sum}")
 
 
-# Using Taking User Input  
+# Using User Input  
 
 num1 = float(input("Enter a Number 1 : "))
 num2 = float(input("Enter a Number 2 : "))
 sum = num1 + num2 
 print(f"The Sum of {num1} + {num2} is {sum}")
+
+
+
+# Question - 2 
+# Python Program to Print Hello World 
+
+print("Hello World")
