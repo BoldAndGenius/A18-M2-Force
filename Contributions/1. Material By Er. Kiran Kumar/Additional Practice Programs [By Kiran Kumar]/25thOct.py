5850b3694ccef8e4358e6f073717a94dbded5ed0
Er. Kiran Kumar <kk9840042@gmail.com>
2024-10-25 02:09:13 +0530

    Program to add two numbers using User Input

--- Contributions/1. Material By Er. Kiran Kumar/Additional Practice Programs [By Kiran Kumar]/25thOct.py
+++ Contributions/1. Material By Er. Kiran Kumar/Additional Practice Programs [By Kiran Kumar]/25thOct.py
@@ -9,4 +9,9 @@ sum = num1 + num2
 print(f"Sum of {num1} + {num2} is {sum}")
 
 
-# Using Taking User Input 
+# Using Taking User Input  
+
+num1 = float(input("Enter a Number 1 : "))
+num2 = float(input("Enter a Number 2 : "))
+sum = num1 + num2 
+print(f"The Sum of {num1} + {num2} is {sum}")
