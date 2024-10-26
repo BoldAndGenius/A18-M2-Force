6fff138036ffb168b7850b3fa8e4178a8d6d936c
Er. Kiran Kumar <kk9840042@gmail.com>
2024-10-26 17:31:47 +0530

    Python Program to display powers of 2 Using Anonymous Function

--- Contributions/1. Material By Er. Kiran Kumar/Additional Practice Programs [By Kiran Kumar]/25thOct.py
+++ Contributions/1. Material By Er. Kiran Kumar/Additional Practice Programs [By Kiran Kumar]/25thOct.py
@@ -828,3 +828,17 @@ sum = 0
 for num in range(1,n+1):
    sum = sum + num 
 print(sum)
+
+
+
+
+# Question - 33 
+# Python Program to display powers of 2 Using Anonymous Function 
+
+# Anonymous Function is same as Lambda Function. Whenever you need to create a temporary function, use anonymous function. Take less lines of code.
+
+
+power = lambda pow: 2**pow 
+print(power(5))
+   
+   
