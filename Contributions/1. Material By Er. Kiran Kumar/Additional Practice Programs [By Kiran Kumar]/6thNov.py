425277bdf940153e15cf659bb7a9595b8e20f2d0
Er. Kiran Kumar <kk9840042@gmail.com>
2024-11-06 03:34:11 +0530

    __dict__ method in Object Oriented Programming System

--- Contributions/1. Material By Er. Kiran Kumar/Additional Practice Programs [By Kiran Kumar]/6thNov.py
+++ Contributions/1. Material By Er. Kiran Kumar/Additional Practice Programs [By Kiran Kumar]/6thNov.py
@@ -9,3 +9,73 @@ class Human:
     
 sannidhi = Human()  
 # output  - Object is Created
+# we can explicitly call the __init__ method as well
+
+
+
+
+
+
+
+
+
+
+
+
+class Human:
+    hands = 2
+    eyes = 2 
+    bones = 206 
+    
+    # we will use this to store data, at the time of object creation (beneficial for us)
+    def __init__(self,name,age):
+        self.name = name 
+        self.age = age 
+    def happyness(self,num):
+        print(f"Feeling Happy {num}%")
+    def sadness(self,num):
+        print(f"Feeling Sad {num}%")
+        
+    
+'''
+ClassDict = {
+    "hands":2,
+    "eyes":2,
+    "bones":206,
+    
+    "happyness":address1,
+    "sadness":address2,
+    "add_info":address3
+}
+
+
+'''
+    
+sannidhi = Human("Sannidhi Shetty",23)  
+#explicity calling the __init__ method
+sannidhi.__init__("Sannidhi Shetty", 23)  # Human.__init__(sannidhi, "Sannidhi Shetty", 23)
+
+divya = Human("Divya Anaparthi",22)
+
+
+# __dict__   act like a variable
+print(Human.__dict__)  # Output - Class Dictionary.
+print()
+print(sannidhi.__dict__)  # Output - Object Dictionary.
+print(divya.__dict__)   # Output - Object Dictionary.
+
+
+
+
+'''
+Class Dictionary - 
+
+{'__module__': 'builtins', 'hands': 2, 'eyes': 2, 'bones': 206, '__init__': <function Human.__init__ at 0x0000019F3BA72DE0>, 'happyness': <function Human.happyness at 0x0000019F3BA72F20>, 'sadness': <function Human.sadness at 0x0000019F3BA72FC0>, '__dict__': <attribute '__dict__' of 'Human' objects>, '__weakref__': <attribute '__weakref__' of 'Human' objects>, '__doc__': None}
+
+
+Object Dictionary - 
+{'name': 'Sannidhi Shetty', 'age': 23}
+{'name': 'Divya Anaparthi', 'age': 22}
+
+
+'''
