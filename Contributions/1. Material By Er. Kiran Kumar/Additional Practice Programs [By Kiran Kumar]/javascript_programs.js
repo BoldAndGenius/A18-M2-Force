9e23607b3a2456f23d59b081ae40df4e598816e1
Er. Kiran Kumar <kk9840042@gmail.com>
2024-11-14 15:45:01 +0530

    Write a JavaScript Program to find the factorial of 10

--- Contributions/1. Material By Er. Kiran Kumar/Additional Practice Programs [By Kiran Kumar]/javascript_programs.js
+++ Contributions/1. Material By Er. Kiran Kumar/Additional Practice Programs [By Kiran Kumar]/javascript_programs.js
@@ -5,3 +5,17 @@ function sum_of_two_number(num1,num2){
     return num1 + num2
 }
 console.log(sum_of_two_number(10,30));
+
+
+
+// Write a JavaScript Program to find the factorial of 10 
+
+function factorial(num){
+    // var num = 10
+    var fact = 1
+    for(var i = 1; i<=num; i++){
+        fact = fact * i
+    }
+    console.log(fact);
+}
+factorial(10)
