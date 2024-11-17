8d287d5c162d8bb999ad4b2b1ef557ef55152b2a
Er. Kiran Kumar <kk9840042@gmail.com>
2024-11-17 22:45:56 +0530

    String Revision Program - 1

---
+++ Contributions/1. Material By Er. Kiran Kumar/Additional Practice Programs [By Kiran Kumar]/strings.py
@@ -0,0 +1,26 @@
+'''
+
+Here are 10 basic Python string program prompts:
+
+1.  Write a program to reverse a string.
+2.  Write a program to check if a string is a palindrome.
+3.  Write a program to count the number of vowels in a string.
+4.  Write a program to find the length of a string without using the `len()` function.
+5.  Write a program to check if a substring exists within a given string.
+6.  Write a program to concatenate two strings without using the `+` operator.
+7.  Write a program to count the number of words in a string.
+8.  Write a program to convert all characters in a string to uppercase.
+9.  Write a program to replace all spaces in a string with underscores.
+10.  Write a program to extract digits from a string.
+
+
+
+
+'''
+
+
+# 1.  Write a program to reverse a string. 
+
+string = "kiran"
+reverse = string[::-1]
+print(reverse)
