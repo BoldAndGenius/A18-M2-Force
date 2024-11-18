80344f8908ccf03c69be6e9a8c6c939e972683cb
Er. Kiran Kumar <kk9840042@gmail.com>
2024-11-18 11:08:03 +0530

    Access Modifier in Python

--- Contributions/1. Material By Er. Kiran Kumar/Additional Practice Programs [By Kiran Kumar]/getter_setter.py
+++ Contributions/1. Material By Er. Kiran Kumar/Additional Practice Programs [By Kiran Kumar]/getter_setter.py
@@ -15,3 +15,27 @@ print(obj.age) # 23
 # to see the object dictionary
 print(obj.__dict__)  # object dictionary =>  {'name': 'Kiran', 'age': 23}
 print(MyConstructor.__dict__) # class dicionary => {'__module__': 'builtins', '__init__': <function MyConstructor.__init__ at 0x00000254FFB636A0>, 'new_func': <function MyConstructor.new_func at 0x00000254FFB63880>, '__dict__': <attribute '__dict__' of 'MyConstructor' objects>, '__weakref__': <attribute '__weakref__' of 'MyConstructor' objects>, '__doc__': None}
+
+
+
+
+
+
+
+
+
+
+# Access Modifier in OOPs 
+
+class PersonalLife:
+    __pin = "2323@" # private variable 
+    def my_pin(self):  # getter
+        return PersonalLife.__pin
+    def update_mypin(self,new_pin): # setter
+        self.__pin = new_pin # assign the new pin 
+        return self.__pin # now call that new pin
+Kiran = PersonalLife()
+print(Kiran.my_pin())  # 2323@ 
+
+print(Kiran.update_mypin('@#$%^')) # @#$%^
+print(Kiran._PersonalLife__pin)  # @#$%^
