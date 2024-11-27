1df457a8175314ffad9b0cd2c786f80886257cbf
Er. Kiran Kumar <kk9840042@gmail.com>
2024-11-27 18:51:41 +0530

    Object Method

---
+++ Contributions/1. Material By Er. Kiran Kumar/Additional Practice Programs [By Kiran Kumar]/object_method.py
@@ -0,0 +1,29 @@
+
+# Object Methods - self is used as the first argument.
+#                - Can only be accessed through object only. 
+#                - No Decorator is required.
+
+
+
+class Human:
+    eyes = 2 
+    ears = 2
+    def __init__(self, name):
+        self.name = name 
+        
+    # object method -- self is used as a first argument, no decorator is there. Can Only be accessed by the Object.
+    def processing(self):
+        print(f'{self.name} is processing')
+    
+ 
+# Using Object I am calling the Object Method.   
+Kiran = Human('Kiran')
+Kiran.processing() # Kiran is processing
+
+
+# Using Class I am trying to call the Object Method.
+# Human.processing()  - get error
+# TypeError: Human.processing() missing 1 required positional argument: 'self'
+
+
+        
