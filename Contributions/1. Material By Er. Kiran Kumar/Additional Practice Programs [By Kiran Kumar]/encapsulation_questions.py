27ca248bfebd2ffd296c84a15b053f4403c832b5
Er. Kiran Kumar <kk9840042@gmail.com>
2024-12-08 13:50:25 +0530

    Encapsulation Question - 2

--- Contributions/1. Material By Er. Kiran Kumar/Additional Practice Programs [By Kiran Kumar]/encapsulation_questions.py
+++ Contributions/1. Material By Er. Kiran Kumar/Additional Practice Programs [By Kiran Kumar]/encapsulation_questions.py
@@ -23,3 +23,29 @@ class Person:
 Kiran = Person()
 Kiran.getter()
 Kiran.setter("Kunal", 25, "USA")
+
+
+
+
+
+# Question - 2
+# Write a Python program to create a class called BankAccount with private instance variables accountNumber and balance. Provide public getter and setter methods to access and modify these variables. 
+
+
+
+class BankAccount:
+    __accountNumber = 123456789
+    __balance = 10000 
+    
+    def getter(self):
+        print(self.__accountNumber)
+        print(self.__balance)
+    def setter(self, new_account_number, new_balance):
+        self.__accountNumber = new_account_number
+        self.__balance = new_balance
+        print(self.__accountNumber)
+        print(self.__balance)
+
+Kiran = BankAccount()
+Kiran.getter()
+Kiran.setter(987654321, 20000)
