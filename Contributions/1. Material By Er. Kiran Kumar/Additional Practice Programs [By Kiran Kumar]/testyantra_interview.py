d094f597f49eadeff74f2946a5f2943771bbc4f9
Er. Kiran Kumar <kk9840042@gmail.com>
2024-12-15 14:44:13 +0530

    Coding Interview Question - 12

--- Contributions/1. Material By Er. Kiran Kumar/Additional Practice Programs [By Kiran Kumar]/testyantra_interview.py
+++ Contributions/1. Material By Er. Kiran Kumar/Additional Practice Programs [By Kiran Kumar]/testyantra_interview.py
@@ -10,3 +10,15 @@ while given_number > 0:
   given_number = given_number // 10 
   
 print(product)
+
+
+
+# Question - 2 : Factorial of a Number.
+
+number = int(input("Enter a Number : ")) # 5 =>  5*4*3*2*1 
+fact = 1
+while number > 0:
+  fact = fact * number 
+  number = number - 1
+print(fact)
+ 
