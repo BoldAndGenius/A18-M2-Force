c2beb0fecba8c46b806f6671ebf1d6ca58c186c1
Er. Kiran Kumar <kk9840042@gmail.com>
2024-12-15 19:01:15 +0530

    Coding Interview Question - 20

--- Contributions/1. Material By Er. Kiran Kumar/Additional Practice Programs [By Kiran Kumar]/tempCodeRunnerFile.py
+++ Contributions/1. Material By Er. Kiran Kumar/Additional Practice Programs [By Kiran Kumar]/tempCodeRunnerFile.py
@@ -1,15 +1,15 @@
-starting_range = 1
-ending_range = 10 
-
-while starting_range <= ending_range:
-  fact = 1
-  temp = starting_range
-  
-  
-
-  while temp > 0:
-    fact = fact * temp 
-    temp = temp - 1
-  print(fact)
-  
-  starting_range = starting_range + 1
+start = 1
+end = 100 
+count = 0
+for num in range(start, end+1):
+  # print(num)  # 2 
+  if num > 1: 
+    prime = True 
+    for fact in range(2,num):
+      if num % fact == 0:
+        prime = False
+    if prime == True:
+      count = count + 1
+      print(num)
+    
+print(f"Total {count} number of Prime Numbers in between {start} and {end}")
--- Contributions/1. Material By Er. Kiran Kumar/Additional Practice Programs [By Kiran Kumar]/testyantra_interview.py
+++ Contributions/1. Material By Er. Kiran Kumar/Additional Practice Programs [By Kiran Kumar]/testyantra_interview.py
@@ -180,3 +180,30 @@ else:
   
   
 
+
+
+# Question - 10 : Prime Number between 1 to 100 
+# Lovely Question ---(*************) (Revision is must)
+
+
+start = 1
+end = 100 
+count = 0
+for num in range(start, end+1):
+  # print(num)  # 2 
+  if num > 1: 
+    prime = True 
+    for fact in range(2,num):
+      if num % fact == 0:
+        prime = False
+    if prime == True:
+      count = count + 1
+      print(num)
+    
+print(f"Total {count} number of Prime Numbers in between {start} and {end}")
+
+
+num = 5
+for fact in range(2,num):
+  if num % fact == 0:
+    print(num)
