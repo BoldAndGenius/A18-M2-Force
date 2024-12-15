0ae813fd95aee5a61348dd7d9be19c0396ebf67b
Er. Kiran Kumar <kk9840042@gmail.com>
2024-12-15 17:32:57 +0530

    Coding Interview Question - 13

--- Contributions/1. Material By Er. Kiran Kumar/Additional Practice Programs [By Kiran Kumar]/testyantra_interview.py
+++ Contributions/1. Material By Er. Kiran Kumar/Additional Practice Programs [By Kiran Kumar]/testyantra_interview.py
@@ -22,3 +22,33 @@ while number > 0:
   number = number - 1
 print(fact)
  
+
+
+
+
+
+
+# Question - 3 : Fibonacci Series of the given number 
+# Series : 0,1,1,2,3,5.....
+# Index  : 1,2,3,4,5
+
+# means, for number = 3, it should print the series as -  0,1,1 
+
+# Fibonacci Series means, first and last number is 0 & 1 respectively, and the next term will be addition of previous two terms.
+
+given_number = int(input("Enter a Number : ")) # 3 
+index = 1
+first = 0
+second = 1
+third = 0
+
+while index <= given_number:
+  print(first)
+  third = first + second 
+  first = second 
+  second = third 
+  index = index + 1
+  
+  
+
+
