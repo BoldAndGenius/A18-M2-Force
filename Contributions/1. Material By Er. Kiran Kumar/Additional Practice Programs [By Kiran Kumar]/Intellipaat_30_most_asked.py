bb909cd1c7379166530bd931b57f02cfe39270e4
Er. Kiran Kumar <kk9840042@gmail.com>
2024-12-16 10:53:44 +0530

    Technical Interview Question - 1

---
+++ Contributions/1. Material By Er. Kiran Kumar/Additional Practice Programs [By Kiran Kumar]/Intellipaat_30_most_asked.py
@@ -0,0 +1,39 @@
+# 30 Most Asked Python Interview Questions 
+
+
+
+
+# Question - 1
+# What is your most preferred laguage  - Python 
+
+# What makes Python different from other languages ?  or  # Why Python? 
+# They want to know, why the developer choose this language. 
+
+
+'''
+
+Python is an interpreted language.
+Dynamic.
+High Level Programming Language.
+It's an Object Oriented Language.
+Easy to understand & learn.
+Easy syntax.
+Automatic & Effective Memory Management.   (automatic garbage collection.)
+
+
+
+Dynamic means, the type of the variable is determined only during run time, Hence one variable can have different data types at different times. Data Types are checked during execution. 
+
+High level language means, that python is easy to read, manages memory for you, works on any system. 
+
+Provides tools that let you focus on solving problems, instead of dealing with hardware.
+
+
+'''
+
+
+
+
+
+
+
