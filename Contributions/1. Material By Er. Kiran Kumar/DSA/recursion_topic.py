a6829c303337dff34474e53c223204c587cd353c
Er. Kiran Kumar <kk9840042@gmail.com>
2025-01-28 16:48:10 +0530

    Print 1 to N Numbers Using Recursion

--- Contributions/1. Material By Er. Kiran Kumar/DSA/recursion_topic.py
+++ Contributions/1. Material By Er. Kiran Kumar/DSA/recursion_topic.py
@@ -2,9 +2,22 @@
 
 
 N = 5 
-def Print1ToN(N): 
-  if N == 0:
+def PrintNTo1(N): 
+  if N <= 0:
     return None 
   print(N)
+  PrintNTo1(N-1)
+PrintNTo1(N)
+
+
+
+
+# Print 1 to N Numbers Using Recursion
+
+N = 5 
+def Print1ToN(N):
+  if N <= 0: 
+    return None 
   Print1ToN(N-1)
+  print(N)
 Print1ToN(N)
